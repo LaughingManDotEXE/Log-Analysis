ce03c8b3f8a9d6ffce42f6e924c06b6591559b9b
LaughingManDotEXE <35981510+LaughingManDotEXE@users.noreply.github.com>
2018-05-01 23:14:26 -0500

    Draft 1
    The python code ran, ans text file is saved. meets PEP8

--- log-analysis.py
+++ log-analysis.py
@@ -1,45 +1,57 @@
 #!/usr/bin/env python
 
+# need psycopg2 to handle postgresql
 import psycopg2
 
-database_name = "news"
+News_Database = "news"
 
-# Store queries into variables
+# created question variables. These will be printed later.
+question_1 = "What are the most popular three articles of all time?"
+question_2 = "Who are the most popular article authors of all time?"
+question_3 = "On which days did more than 1% of requests lead to errors?"
+
+# created answer variables, which are PostgreSQL statements
+answer_1 = """SELECT title, COUNT(*) as article_views FROM articles \n
+              JOIN log ON articles.slug = SUBSTRING(log.path, 10)\n
+              GROUP BY title ORDER BY article_views DESC LIMIT 3;"""
+
+answer_2 = """SELECT authors.name, COUNT(*) as author_rank \n
+              FROM articles JOIN authors ON articles.author = authors.id \n
+              JOIN log ON articles.slug = substring(log.path, 10)\n
+              WHERE log.status = '200 OK' GROUP BY authors.name \n
+              ORDER BY author_rank DESC;"""
+
+answer_3 = "SELECT * FROM error_view WHERE \"Error_Percentage\" > 1;"
 
 
-# Store results
+# returns query result
+def get_query_Results(query):
+    db = psycopg2.connect(database=News_Database)
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
 
 
-
-# Get query results from databse and return them
-
-
-
-# Print results method
-
-
-# Store query results
+# print_results will show the answer run, formatted, and line down
+def print_results(answers):
+    for i in range(len(answers)):
+        title = answers[i][0]
+        res = answers[i][1]
+        print("\t" + "%s - %d" % (title, res))
+    print("\n")
 
 
-# Print query results
+# Run get_query_Results and store into result variables
+result_1 = get_query_Results(answer_1)
+result_2 = get_query_Results(answer_2)
+result_3 = get_query_Results(answer_3)
 
-# create views because third question is difficult
-
-question 1 - What are the most popular three articles of all time?
-databases needed are articles and log
-select article title where log method GET is highest
-print top 3 in desc (based on GET)
-
-question_1 = SELECT title from articles where 
-JOIN log
-GROUP BY title ORDER BY views DESC LIMIT 3
-
-question 2 -  Who are the most popular article authors of all time?
-need all databases
-join articles and authors
-select authors where log method GET is highest
-print top 3 desc (based on GET)
-
-question 3 - On which days did more than 1% of requests lead to errors?
-On which days did more than 1% of requests lead to errors?
-errors is log status 400 +
+# Print all Questions and Answers.
+print(question_1)
+print_results(result_1)
+print(question_2)
+print_results(result_2)
+print(question_3)
+print_results(result_3)
